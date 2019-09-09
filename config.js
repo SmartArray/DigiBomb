73c0788aa0009a23ab93cbbd1d296f6f53fa6758
Yoshi <yoshi@jaeger.berlin>
2019-09-09 14:13:36 +0200

    Config changed

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	REQUEST_AMOUNT: 1,
-	ADDRESSES: 30,
+	ADDRESSES: 1000,
 	EACH: 0.0003, // final amount will be less due to fees
 	HOST: 'https://digiexplorer.info/api',
 	RECEIVE_ADDRESS: 'DM3DWMn7tnV4kcf8norDw1zxvktY4GPPhk',
-  MAX_OUTPUTS: 10,
+  MAX_OUTPUTS: 90,
 }
 
